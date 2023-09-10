81eb5f312f490ca99d7ff4dd9f4a2a8e4ace067e
Nguyen Hoang Phuc Huy <hoangphuchuy.nguyen@zuhlke.com>
2023-09-10 14:35:31 +0700

    Initialize

---
+++ api/__init__.py
@@ -0,0 +1,44 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv(".env")
+
+app = Flask(__name__)
+
+
+db_address = os.environ.get("DB_ADDRESS")
+db_name = os.environ.get("DB_NAME")
+db_port = "5432"
+postgres_username = os.environ.get("POSTGRES_USERNAME")
+postgres_password = os.environ.get("POSTGRES_PASSWORD")
+
+
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = f"postgresql://{postgres_username}:{postgres_password}@{db_address}:{db_port}/{db_name}"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+from api.todo.models import Todo
+from api.user.models import User
+
+
+# no need to set-up database during image build process
+if os.environ.get("ENV") != "CI":
+    db.create_all()
+    db.session.commit()
+
+
+@app.route("/ping")
+def ping():
+
+    return "pong!"
+
+
+from api.todo import views
+from api.user import views
---
+++ api/todo/views.py
@@ -0,0 +1,67 @@
+from api import app
+from api.todo.models import Todo
+from flask import jsonify, request
+from api import db
+
+
+@app.route("/todos", methods=["GET"])
+def list_todos():
+    try:
+        todos = Todo.query.all()
+        return jsonify(todos), 200
+    except:
+        return "Error fetching data", 500
+
+
+@app.route("/todos/<int:id>", methods=["GET"])
+def get_todo(id):
+    try:
+        todo = Todo.query.get_or_404(id)
+        return jsonify(todo), 200
+    except:
+        return "Not found", 404
+
+
+@app.route("/todos", methods=["POST"])
+def add_todo():
+    name = request.json["name"]
+    description = request.json["description"]
+    user_id = request.json["user_id"]
+
+    new_todo = Todo(name=name, description=description, user_id=user_id)
+
+    try:
+        db.session.add(new_todo)
+        db.session.commit()
+        return jsonify(new_todo), 201
+    except:
+        return "Error creating todo", 500
+
+
+@app.route("/todos/<int:id>", methods=["DELETE"])
+def delete_todo(id):
+    try:
+        todo = Todo.query.get_or_404(id)
+        db.session.delete(todo)
+        db.session.commit()
+        return "OK", 200
+    except:
+        return "Error deleting todo", 500
+
+
+@app.route("/todos/<int:id>", methods=["PUT"])
+def update_todo(id):
+    name = request.json["name"]
+    description = request.json["description"]
+    completed = request.json["completed"]
+
+    try:
+        todo = Todo.query.get_or_404(id)
+        todo.name = name
+        todo.description = description
+        todo.completed = completed
+
+        db.session.commit()
+        return "OK", 200
+    except:
+        return "Error updating todo", 500
